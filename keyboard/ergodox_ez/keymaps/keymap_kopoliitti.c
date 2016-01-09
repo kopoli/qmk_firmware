33f9d34e2e55d19113eeb55f753dc20f3d5811db
Kalle Kankare <kalle.kankare@iki.fi>
2016-01-09 21:10:03 +0200

    keymaps/kopoliitti: add < to the bottom row
    This is just for testing to see if the bottom row having shifted by one
    causes any problems.

--- keyboard/ergodox_ez/keymaps/keymap_kopoliitti.c
+++ keyboard/ergodox_ez/keymaps/keymap_kopoliitti.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | CAPS   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
  * |--------+------+------+------+------+------|  L1  |           |  L1  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |   <  |   Z  |   X  |   C  |   V  |      |           |      |   B  |   N  |   M  |   ,  |./Ctrl|//RShift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LCtrl | LGui | LAlt | <    | [    |                                       | ~L1 | Left |  Up  | Down | Right |
  *   `----------------------------------'                                       `----------------------------------'
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(MDIA,KC_GRV),KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_INS,
         KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   TG(1),
+        KC_LSFT,        KC_NUBS,      KC_Z,   KC_X,   KC_C,   KC_V,   TG(1),
         KC_LCTL,        KC_LGUI,      KC_LALT,KC_NUBS,KC_LBRC,
                                               ALT_T(KC_APP),  KC_RBRC,
                                                               KC_HOME,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_BSPC,
              KC_DEL,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
-             TG(1),       KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             TG(1),       KC_B,   KC_N,   KC_M,   KC_COMM,CTL_T(KC_DOT),    SFT_T(KC_SLSH),
                                   KC_FN1, KC_LEFT,KC_UP,  KC_DOWN,          KC_RIGHT,
              KC_RALT,        CTL_T(KC_ESC),
              KC_PGUP,

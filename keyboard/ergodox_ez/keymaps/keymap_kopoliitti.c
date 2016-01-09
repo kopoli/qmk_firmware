c088aeb37adb3a4fd6e7747ac470b227a08332da
Kalle Kankare <kalle.kankare@iki.fi>
2016-01-09 21:59:39 +0200

    keymaps/kopoliitti: change L1 keys to nordic variant

--- keyboard/ergodox_ez/keymaps/keymap_kopoliitti.c
+++ keyboard/ergodox_ez/keymaps/keymap_kopoliitti.c
@@ -1,6 +1,7 @@
 #include "ergodox_ez.h"
 #include "debug.h"
 #include "action_layer.h"
+#include "keymap_extras/keymap_nordic.h"
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
@@ -75,19 +76,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
        // left hand
        KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       KC_TRNS,KC_EXLM,NO_AT,  NO_LCBR,NO_RCBR,NO_PIPE,KC_TRNS,
+       KC_TRNS,KC_HASH,NO_DLR, NO_LPRN,NO_RPRN,NO_GRV,
+       KC_TRNS,KC_PERC,NO_CIRC,NO_LBRC,NO_RBRC,NO_TILD,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_INS,
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_LBRC,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_RBRC,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    LSFT(KC_BSLS), KC_LBRC,
+                KC_DOWN, KC_4,   KC_5,    KC_6,    NO_PLUS, KC_RBRC,
+       KC_TRNS, NO_AMP,  KC_1,   KC_2,    KC_3,    ALGR(NO_PLUS), KC_TRNS,
+                         KC_TRNS,KC_DOT,  KC_0,    NO_EQL,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS

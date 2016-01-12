5d93b7978cf83e020d1f487a50f785520386dd3c
Kalle Kankare <kalle.kankare@iki.fi>
2016-01-12 21:10:05 +0200

    keymaps/kopoliitti: fix switching to layer-1
    The KC_FN1 had a problem when there was two keys bound to this code. The
    layer 1 could get stuck sometimes when the keys were pressed
    simultaneously.
    
    The MO(1) approach does not seem to have the same limitation.

--- keyboard/ergodox_ez/keymaps/keymap_kopoliitti.c
+++ keyboard/ergodox_ez/keymaps/keymap_kopoliitti.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_NUBS,      KC_Z,   KC_X,   KC_C,   KC_V,   KC_NO,
-        KC_LCTL,        KC_LGUI,      KC_LALT,KC_NO,  KC_FN1,
+        KC_LCTL,        KC_LGUI,      KC_LALT,KC_NO,  MO(1),
                                               ALT_T(KC_APP),  KC_PAUS,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_NO,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
              KC_NO,       KC_B,   KC_N,   KC_M,   KC_COMM,CTL_T(KC_DOT),    SFT_T(KC_SLSH),
-                                  KC_FN1, KC_LEFT,KC_UP,  KC_DOWN,          KC_RIGHT,
+                                  MO(1),  KC_LEFT,KC_UP,  KC_DOWN,          KC_RIGHT,
              KC_DEL,      CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_RALT, KC_ENT

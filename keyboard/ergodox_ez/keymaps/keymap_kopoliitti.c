9838cd3aea8366c46f97cd808e6455590a9d7baf
Kalle Kankare <kalle.kankare@vincit.fi>
2016-01-19 08:06:45 +0200

    keymap/kopoliitti: add B to the left side
    This is an experiment.

--- keyboard/ergodox_ez/keymaps/keymap_kopoliitti.c
+++ keyboard/ergodox_ez/keymaps/keymap_kopoliitti.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | CAPS   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LShift |   <  |   Z  |   X  |   C  |   V  |      |           |      |   B  |   N  |   M  |   ,  |./Ctrl|//RShift|
+ * | LShift |   <  |   Z  |   X  |   C  |   V  |  B   |           |      |   N  |   M  |   ,  |  .   |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LCtrl | LGui | LAlt | ~L3  |  ~L1 |                                       | ~L1 | Left | Down |  Up  | Right |
  *   `----------------------------------'                                       `----------------------------------'
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(MDIA,KC_GRV),KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
         KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_NUBS,      KC_Z,   KC_X,   KC_C,   KC_V,   KC_NO,
+        KC_LSFT,        KC_NUBS,      KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,
         KC_LCTL,        KC_LGUI,      KC_LALT,MO(3),  MO(1),
                                               ALT_T(KC_APP),  KC_PAUS,
                                                               KC_HOME,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_BSPC,
              KC_NO,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
-             KC_NO,       KC_B,   KC_N,   KC_M,   KC_COMM,CTL_T(KC_DOT),    SFT_T(KC_SLSH),
+             KC_NO,       KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   MO(1),  KC_LEFT,KC_DOWN,KC_UP,            KC_RIGHT,
              KC_DEL,      CTL_T(KC_ESC),
              KC_NO,

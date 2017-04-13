f3c0b9094d12e150c3b3f2b149ee92e7a6442e6f
Kalle Kankare <kalle.kankare@vincit.fi>
2017-04-13 16:03:52 +0300

    keymaps/kopoliitti: add ENTER with RSHIFT

--- keyboards/ergodox/ez/keymaps/kopoliitti/keymap.c
+++ keyboards/ergodox/ez/keymaps/kopoliitti/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | CAPS   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LShift |   <  |   Z  |   X  |   C  |   V  |  B   |           |      |   N  |   M  |   ,  |  .   |//Ctrl| RShift |
+ * | LShift |   <  |   Z  |   X  |   C  |   V  |  B   |           |      |   N  |   M  |   ,  |  .   |//Ctrl| RSh/Ent|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LCtrl | LGui | LAlt | ~L3  |  ~L1 |                                       | ~L1 | Left | Down |  Up  | Right |
  *   `----------------------------------'                                       `----------------------------------'
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_BSPC,
              KC_NO,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
-             KC_NO,       KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             KC_NO,       KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   RSFT_T(KC_ENT),
                                   MO(1),  KC_LEFT,KC_DOWN,KC_UP,            KC_RIGHT,
              KC_DEL,      CTL_T(KC_ESC),
              KC_NO,
